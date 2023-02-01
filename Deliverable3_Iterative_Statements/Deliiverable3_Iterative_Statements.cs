5eff7de3efd34deb4570842d74e246278418025e
jakeb <jakeb@DESKTOP-I85EH55>
2023-01-31 20:17:35 -0500

    Add project files.

---
+++ Deliverable3_Iterative_Statements/Deliiverable3_Iterative_Statements.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Deliverable3_Iterative_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            string series = "";
+
+            while (true)
+            {
+                Console.WriteLine("Enter an integer between 1 and 100: ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input >= 1 && input <= 100)
+                {
+                    break;
+                }
+            }
+            
+            while (true)
+            {
+                Console.Write("Specify the series type: Even or Odd. ");
+                series = Console.ReadLine();
+
+                if (series == "Even" || series == "Odd")
+                {
+                    break;
+                }
+            }
+
+            if (series == "Odd")
+            {
+                Console.Write("You have selected " + series + " series. The numbers between 0 and "
+                    + input + " are: ");
+                Console.WriteLine();
+                for (int i = 1; i < input; i = i + 2) 
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine(input);
+            }
+
+            if (series == "Even")
+            {
+                Console.Write("You have selected " + series + " series. The numbers between 0 and "
+                    + input + " are: ");
+                for (int i = 0; i < input; i = i + 2)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine(input);
+            }
+
+        }
+    }
+}
